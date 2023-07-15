8f82aa6445541050f2b36c7061029d6b0e6d0d8f
Rajakumar Battula <rkb0940@gmail.com>
2023-07-15 23:09:29 +0530

    modified
    Signed-off-by: Rajakumar Battula <rkb0940@gmail.com>

--- Day1/Excercise/Hello/Hello.go
+++ Day1/Excercise/Hello/Hello.go
@@ -5,6 +5,8 @@ import "fmt" //in built package (used for outputting data/formattin data)
 //Every executable program should have entry point
 
 func main() {
+	// name := "PSL"
+	// fmt.Printf("%T",name)
 
 	fmt.Println("Hello World....")
 	test()
